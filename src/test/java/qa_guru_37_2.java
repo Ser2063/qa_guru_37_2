3d10db9792966ff3d4ec6e1451209991539733ff
Ser2063 <jca@mail.ru>
2025-09-15 21:44:03 +0300

    added xPath 2

--- src/test/java/qa_guru_37_2.java
+++ src/test/java/qa_guru_37_2.java
@@ -20,7 +20,7 @@ public class qa_guru_37_2 {
     @Test
     void qa_guru_37_2() {
         open("/automation-practice-form");
-        $x("//input[@id='firstName']").setValue("Sergey"); // XPath
+        $x("//input[@id='firstName']").setValue("Sergey");
         $x("//input[@id='lastName']").setValue("Rzhevsky");
         $x("//input[@id='userEmail']").setValue("jca@mail.ru");
         $x("//label[@for='gender-radio-1']").click();
@@ -29,8 +29,7 @@ public class qa_guru_37_2 {
         $x("//select[@class='react-datepicker__month-select']").selectOption("January");
         $x("//select[@class='react-datepicker__year-select']").selectOption("1984");
         $x("//div[contains(@class, 'react-datepicker__day') and text()='8' and not(contains(@class, 'outside-month'))]").click();
-        $x("//input[@id='dateOfBirthInput']").setValue("08 Jan 1984").pressEnter();
-        $x("//input[@id='subjectsInput']").setValue("Тема");
+        $x("//input[@id='subjectsInput']").setValue("C").pressEnter();
         $x("//textarea[@id='currentAddress']").setValue("Адрес");
     }
 }
